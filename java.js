a28ff8135a70e71b7a4e7775097d1174d2e9e836
bila9050 <nabilanurabidah009@gmail.com>
2024-12-20 16:46:33 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+document.write("<hr width=110% color=red>");
+document.writeln("<h1 align=center>WEBSITE Profile <font color=pink> Nabila Nur A'bidah </font></h1>");
+document.write("<hr width=110% color=red>");
+document.writeln("<div align=center> <img src=profil.jpg width=30% height=30% alt=foto></div>");
+
+document.writeln("<h2>Biodata : </h2>");
+document.writeln("<ol type=1 style='font-size:smaller ; font-weight:normal;>");
+document.writeln("<li> Nim : 241011700634 </li>");
+document.writeln("<li> Nama : Nabila Nur A'bidah </li>");
+document.writeln("<li> Kelas : 01SIFP016 </li>");
+document.writeln("</ol>");
+document.writeln("<ul> type=circle style='font-size:smaller ; font-weight:normal;'>")
+document.writeln("<li> Program Studi : Sistem Informasi </li>")
+document.writeln("<li> Alamat : Kp.Ciwilan,Desa Citeras,Rangkasbitung</li>")
+document.writeln("<li> Asal Sekolah : SMK NEGERI 1 RANGKASBITUNG </li>")
+document.writeln("</ul>")
+document.writeln("<ol type=i style='font-size:smaller ; font-weight:normal;'>")
+document.writeln("<li> Jurusan : Teknik Jaringan Komputer dan Telekomunikasi </li>")
+document.writeln("<li> Email : nabilanurabidah2@gmail.com </li>")
+document.writeln("<li> Telepon : 0838-9012-4626 </li>")
+document.writeln("</ol>")
+
+document.writeln("<hr width=110% color=red>");
+document.writeln("<p align=center>Design by Nabila nur abidah &copy; 2024 AllRight Reserved</p>")
+document.writeln("<hr width=110% color=red>");
